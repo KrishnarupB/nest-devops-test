344c09016fe979da9ec0c960959bdea6b3082a8d
KrishnarupB <85047556+KrishnarupB@users.noreply.github.com>
2021-06-14 20:05:17 +0530

    1

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,13 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { CarService } from './car.service';
+import { CarDto } from './car,dto';
 
 @Controller('car')
 export class CarController {
-constructor(private carService: CarService){}
+  constructor(private carService: CarService) {}
 
-@Get()
-async getCars(){
-return this.carService.getCars();    
-}
+  @Get()
+  public getCars() {
+    return this.carService.getCars();
+  }
 
+  @Post()
+  public postCar(@Body() car: CarDto) {
+    return this.carService.postCar(car);
+  }
+
+  @Get(':id')
+  public getCarByID(@Param('id') id: number) {
+    return this.carService.getCarByID(id);
+  }
+
+  @Delete(':id')
+  public deleteCarByID(@Param('id') id: number) {
+    return this.carService.deleteCarByID(id);
+  }
+
+  @Put(':id')
+  public async putCarByID(@Param('id') id: number, @Query() query) {
+    const propertyName = query.property_name;
+    const propertyValue = query.property_value;
+    return this.carService.putCarByID(id, propertyName, propertyValue);
+  }
 }
