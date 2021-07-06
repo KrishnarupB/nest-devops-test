ce12e16f4894d133676f1711e747e31ca4873618
KrishnarupB <85047556+KrishnarupB@users.noreply.github.com>
2021-07-06 16:34:18 +0530

    1

--- src/car/cars.mock.ts
+++ src/car/cars.mock.ts
@@ -1,9 +1,8 @@
-export const CARS=[
-
-    {
-        id:1,
-        brand:'BMW',
-        color:'Gold',
-        model:'BMW X5'
-    },
+export const CARS = [
+  {
+    id: 1,
+    brand: 'BMW',
+    color: 'Gold',
+    model: 'BMW X5',
+  },
 ];
