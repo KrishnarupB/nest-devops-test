4c4927b6d4f84beeffaa5272792e786b3a8491e9
KrishnarupB <85047556+KrishnarupB@users.noreply.github.com>
2021-07-06 13:53:04 +0530

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CarModule } from './car/car.module';
 import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [CarModule, ConfigModule.forRoot()],
+  imports: [
+    CarModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+  })],
 })
 export class AppModule {}
